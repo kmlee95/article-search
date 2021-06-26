ba570eff7d33b47a7a8d6f640d7e3995c0ef9825
kmlee95 <kmlee95@naver.com>
2021-06-26 21:58:24 +0900

    feat #3 : home 초기설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '@/pages/Home';
+
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
