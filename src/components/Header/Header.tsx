643f5efb157584ff9651f543e9ff53da2db85e52
kmlee95 <kmlee95@naver.com>
2021-07-06 00:49:50 +0900

    feat #4 : 즐겨찾기 페이지 구성

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,27 @@
+import { useCallback, useState } from 'react';
 import { Menu } from 'antd';
 import { MailOutlined, AppstoreOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = ({}) => {
+  const [current, setCurrent] = useState('main');
+
+  const onClickMenu = useCallback(
+    (e) => {
+      setCurrent(e.key);
+    },
+    [current],
+  );
+
   return (
-    <Menu mode="horizontal">
+    <Menu mode="horizontal" onClick={onClickMenu} selectedKeys={[current]}>
       <Menu.Item key="main" icon={<MailOutlined />}>
-        메인이동
+        <Link to="/">전체 기사 목록</Link>
       </Menu.Item>
       <Menu.Item key="favorites" icon={<AppstoreOutlined />}>
-        즐겨찾기 목록확인
+        <Link to="/Favorite">즐겨찾기 목록</Link>
       </Menu.Item>
     </Menu>
   );
